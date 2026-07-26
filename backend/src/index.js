f6eb27a775ea7a11c11464b4dc252bc15837d13c
kushB645 <kushb545@gmail.com>
2026-07-26 20:21:39 +0530

    connected the db

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,12 @@
+require('dotenv').config();
+
 const app = require("./app");
+const { default: dbConnection } = require('./db/index.js');
 
 const port = 3000;
 
 app.listen(port , () => {
     console.log("server is running at port " + port);
 });
+
+dbConnection();
