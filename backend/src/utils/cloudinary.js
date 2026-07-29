0fb04458c64d138938229e62d91ed87739ec0f9f
kushB645 <kushb545@gmail.com>
2026-07-29 15:29:41 +0530

    added multer and cloudinary

--- backend/src/utils/cloudinary.js
+++ backend/src/utils/cloudinary.js
@@ -1,8 +1,25 @@
-import { v2 as cloudinary } from 'cloudinary'
-import fs from "fs"
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
 
-cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_NAME, 
-  api_key: process.env.CLOUDINARY_API_KEY, 
-  api_secret: process.env.CLOUDINARY_API_SECRET
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
+const uploadOnCloudinary = async function (localFilePath) {
+  try {
+    if (!localFilePath) return null;
+    //uploading post on the cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto", // auto will automatically decide it is jpg,mp4,mp3,pdf
+    });
+
+    console.log("file is uploaded", response.url);
+
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); //it will remove the post from local as it fail
+    return null;
+  }
+};
